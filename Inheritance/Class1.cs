a581735bef28ad231ad2d22a5eea92608bde9f00
pcuser <pcuser@INFCH03677>
2020-01-31 12:34:17 +0530

    Add project files.

---
+++ Inheritance/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string s1 = " Cshr ";
+            string s2 = s1.Insert(3, " a ");
+            string s3 = s2.Insert(5, " p ");
+            for (int i = 0; i < s3.Length; i++)
+                Console.WriteLine(s3[i]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Organisation
+    {
+        static protected string OrgName="LTI";
+         public  int RegNo;
+
+      internal  Organisation(int RegNo)
+        {
+            this.RegNo = RegNo;
+            Console.WriteLine("Organisation COnstruction");
+        }
+
+        internal protected void DisplayOrgDetails()
+        {
+            Console.WriteLine("OrganizationName={0} ,RegistrationNumber={1}",OrgName,RegNo);
+        }
+        ~Organisation()
+        {
+            Console.WriteLine(" Organisation Destructor");
+        }
+    }
+
+
+    class Employeedetails : Organisation
+    {
+        internal int Empid;
+       internal string EmpName;
+
+
+        internal Employeedetails(int RegNo, int Empid, string EmpName) : base(RegNo)
+        {
+            this.RegNo = RegNo;
+            this.Empid = Empid;
+            this.EmpName = EmpName;
+            Console.WriteLine("Employee Details");
+
+        }
+        internal void Display()
+        {
+            Console.WriteLine("Company Register Name={0}, EmployeeId={1},EmployeeName={2}",RegNo,Empid,EmpName);
+            
+        }
+        ~Employeedetails()
+        {
+            Console.WriteLine("Employeedetails destructor");
+        }
+
+    }
+
+    class Project: Employeedetails
+    {
+        string Activity;
+        internal Project(int RegNo, int Empid, string EmpName,string activity) : base(RegNo,Empid,EmpName)
+        {
+            this.RegNo = RegNo;
+            this.Empid = Empid;
+            this.EmpName = EmpName;
+            this.Activity = activity;
+            Console.WriteLine("Project Details");
+
+        }
+        ~Project()
+        {
+            Console.WriteLine("Project Destructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Project p = new Project(1001, 1, "vivek", "play");
+            p.Display();
+            p.DisplayOrgDetails();
+            Console.Read();
+        }
+    }
+}
